70fb64a263b801c862889a66768b49a5116b12a2
Thanh Truong <thanhtnna@gmail.com>
2018-05-09 00:04:42 +0700

    add more script

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,136 +1,75 @@
 #!/bin/bash
-#
-# Copyright  2017  Johns Hopkins University (Author: Shinji Watanabe, Yenda Trmal)
-# Apache 2.0
 
-# Begin configuration section.
-mictype=worn # worn, ref or others
-cleanup=true
-# End configuration section
-. ./utils/parse_options.sh  # accept options.. you can run this run.sh with the
+# 03.05.2018 thanh.truong 
+# prepare data for waw.scp, text, spk2utt
 
-. ./path.sh
+# Setup relevant folders
+WAV_ROOT="/home/kinkin/workspace/kaldi/02.source/kaldi/egs/mytrain/wav"
+TRAIN_ROOT="/home/kinkin/workspace/kaldi/02.source/kaldi/egs/mytrain/data/train"
+utils="utils"
 
-echo >&2 "$0" "$@"
-if [ $# -ne 3 ] ; then
-  echo >&2 "$0" "$@"
-  echo >&2 "$0: Error: wrong number of arguments"
-  echo -e >&2 "Usage:\n  $0 [opts] <audio-dir> <json-transcript-dir> <output-dir>"
-  echo -e >&2 "eg:\n  $0 /corpora/chime5/audio/train /corpora/chime5/transcriptions/train data/train"
-  exit 1
+# Setup wav folders
+if [ ! -d $WAV_ROOT ]; then
+  echo "Cannot find wav directory $WAV_ROOT"
+  exit 1;
+fi
+if [ ! -d $TRAIN_ROOT ]; then
+  echo "Cannot find wav directory $TRAIN_ROOT"
+  exit 1;
 fi
 
-set -e -o pipefail
+echo "Preparing data sets"
 
-adir=$1
-jdir=$2
-dir=$3
+# Create wav.scp files
+scp="$TRAIN_ROOT/wav.scp"
+rm -f "$scp"
+echo "Creating $scp"
+for d in $(find $WAV_ROOT -maxdepth 1 -type d )
+do
+	allfile=`ls $d/`
+	for one in $allfile
+	do
+		
+		if [ ${one: -4} == ".wav" ]; then
+			
+			name=`echo "$one" | sed s/.wav//`
+			path="$d/$one"		
+			name=`echo -e "$name\t\t$path"`
 
-json_count=$(find $jdir -name "*.json" | wc -l)
-wav_count=$(find $adir -name "*.wav" | wc -l)
+			echo $name >> temp.txt
+		fi
+	done
+	
+done
+sort temp.txt >> $scp
+rm -f temp.txt
 
-if [ "$json_count" -eq 0 ]; then
-  echo >&2 "We expect that the directory $jdir will contain json files."
-  echo >&2 "That implies you have supplied a wrong path to the data."
-  exit 1
-fi
-if [ "$wav_count" -eq 0 ]; then
-  echo >&2 "We expect that the directory $adir will contain wav files."
-  echo >&2 "That implies you have supplied a wrong path to the data."
-  exit 1
-fi
+#Create text file
+textfile="$TRAIN_ROOT/text"
+rm -f "$textfile"
+echo "Creating $textfile"
+for d in $(find $WAV_ROOT -maxdepth 1 -type d -printf '%f\n')
+do
+  script_file="$d" 
+  script_file+="_script.txt"
+  
+  script_path=$WAV_ROOT
+  script_path+="/"
+  script_path+="$d"
+  script_path+="/"
+  script_path+=$script_file
+  
+  if [ -f "${script_path}" ]; then
+  
+  	while IFS= read -r line;do
+    	echo "$line" | sed -e 's/[',','.','?']//g' >> temp.txt
+	done < "$script_path"
+  fi
+done
+sort temp.txt >> $textfile
+rm -f temp.txt
 
-echo "$0: Converting transcription to text"
 
-mkdir -p $dir
-for file in $jdir/*json; do
-  ./local/json2text.py --mictype $mictype $file
-done | \
-  sed -e "s/\[inaudible[- 0-9]*\]/[inaudible]/g" |\
-  sed -e 's/ - / /g' |\
-  sed -e 's/mm-/mm/g' > $dir/text.orig
 
-echo "$0: Creating datadir $dir for type=\"$mictype\""
-
-if [ $mictype == "worn" ]; then
-  # convert the filenames to wav.scp format, use the basename of the file
-  # as a the wav.scp key, add .L and .R for left and right channel
-  # i.e. each file will have two entries (left and right channel)
-  find $adir -name  "S[0-9]*_P[0-9]*.wav" | \
-    perl -ne '{
-      chomp;
-      $path = $_;
-      next unless $path;
-      @F = split "/", $path;
-      ($f = $F[@F-1]) =~ s/.wav//;
-      @F = split "_", $f;
-      print "${F[1]}_${F[0]}.L sox $path -t wav - remix 1 |\n";
-      print "${F[1]}_${F[0]}.R sox $path -t wav - remix 2 |\n";
-    }' | sort > $dir/wav.scp
-
-  # generate the transcripts for both left and right channel
-  # from the original transcript in the form
-  # P09_S03-0006072-0006147 gimme the baker
-  # create left and right channel transcript
-  # P09_S03.L-0006072-0006147 gimme the baker
-  # P09_S03.R-0006072-0006147 gimme the baker
-  sed -n 's/  *$//; h; s/-/\.L-/p; g; s/-/\.R-/p' $dir/text.orig | sort > $dir/text
-elif [ $mictype == "ref" ]; then
-  # fixed reference array
-
-  # first get a text, which will be used to extract reference arrays
-  perl -ne 's/-/.ENH-/;print;' $dir/text.orig | sort > $dir/text
-
-  find $adir | grep "\.wav" | sort > $dir/wav.flist
-  # following command provide the argument for grep to extract only reference arrays
-  grep `cut -f 1 -d"-" $dir/text | awk -F"_" '{print $2 "_" $3}' | sed -e "s/\.ENH//" | sort | uniq | sed -e "s/^/ -e /" | tr "\n" " "` $dir/wav.flist > $dir/wav.flist2
-  paste -d" " \
-	<(awk -F "/" '{print $NF}' $dir/wav.flist2 | sed -e "s/\.wav/.ENH/") \
-	$dir/wav.flist2 | sort > $dir/wav.scp
-else
-  # array mic case
-  # convert the filenames to wav.scp format, use the basename of the file
-  # as a the wav.scp key
-  find $adir -name "*.wav" -ipath "*${mictype}*" |\
-    perl -ne '$p=$_;chomp $_;@F=split "/";$F[$#F]=~s/\.wav//;print "$F[$#F] $p";' |\
-    sort -u > $dir/wav.scp
-
-  # convert the transcripts from
-  # P09_S03-0006072-0006147 gimme the baker
-  # to the per-channel transcripts
-  # P09_S03_U01_NOLOCATION.CH1-0006072-0006147 gimme the baker
-  # P09_S03_U01_NOLOCATION.CH2-0006072-0006147 gimme the baker
-  # P09_S03_U01_NOLOCATION.CH3-0006072-0006147 gimme the baker
-  # P09_S03_U01_NOLOCATION.CH4-0006072-0006147 gimme the baker
-  perl -ne '$l=$_;
-    for($i=1; $i<=4; $i++) {
-      ($x=$l)=~ s/-/.CH\Q$i\E-/;
-      print $x;}' $dir/text.orig | sort > $dir/text
-
-fi
-$cleanup && rm -f $dir/text.* $dir/wav.scp.* $dir/wav.flist
-
-# Prepare 'segments', 'utt2spk', 'spk2utt'
-if [ $mictype == "worn" ]; then
-  cut -d" " -f 1 $dir/text | \
-    awk -F"-" '{printf("%s %s %08.2f %08.2f\n", $0, $1, $2/100.0, $3/100.0)}' |\
-    sed -e "s/_[A-Z]*\././2" \
-    > $dir/segments
-elif [ $mictype == "ref" ]; then
-  cut -d" " -f 1 $dir/text | \
-    awk -F"-" '{printf("%s %s %08.2f %08.2f\n", $0, $1, $2/100.0, $3/100.0)}' |\
-    sed -e "s/_[A-Z]*\././2" |\
-    sed -e "s/ P.._/ /" > $dir/segments
-else
-  cut -d" " -f 1 $dir/text | \
-    awk -F"-" '{printf("%s %s %08.2f %08.2f\n", $0, $1, $2/100.0, $3/100.0)}' |\
-    sed -e "s/_[A-Z]*\././2" |\
-    sed -e 's/ P.._/ /' > $dir/segments
-fi
-cut -f 1 -d ' ' $dir/segments | \
-  perl -ne 'chomp;$utt=$_;s/_.*//;print "$utt $_\n";' > $dir/utt2spk
-
-utils/utt2spk_to_spk2utt.pl $dir/utt2spk > $dir/spk2utt
-
-# Check that data dirs are okay!
-utils/validate_data_dir.sh --no-feats $dir || exit 1
+echo "--> Data preparation succeeded"
+exit 0
--- prepare_dict.sh
+++ prepare_dict.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-# Copyright (c) 2018, Johns Hopkins University (Jan "Yenda" Trmal<jtrmal@gmail.com>)
-# License: Apache 2.0
 
-# Begin configuration section.
-# End configuration section
-. ./utils/parse_options.sh
+# Copyright 2010-2012 Microsoft Corporation  
+#           2012-2014 Johns Hopkins University (Author: Daniel Povey)
+#                2015 Guoguo Chen
 
-. ./path.sh
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+# WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+# MERCHANTABLITY OR NON-INFRINGEMENT.
+# See the Apache 2 License for the specific language governing permissions and
+# limitations under the License.
 
-set -e -o pipefail
-set -o nounset                              # Treat unset variables as an error
-
+# Call this script from one level above, e.g. from the s3/ directory.  It puts
+# its output in data/local/.
 
 # The parts of the output of this that will be needed are
 # [in data/local/dict/ ]
@@ -20,105 +28,65 @@ set -o nounset                              # Treat unset variables as an error
 # optional_silence.txt
 # silence_phones.txt
 
+# run this from ../
+dict_suffix=
 
-# check existing directories
-[ $# != 0 ] && echo "Usage: $0" && exit 1;
+echo "$0 $@"  # Print the command line for logging
+. utils/parse_options.sh || exit 1;
 
-dir=data/local/dict
-
+dir=data/local/dict${dict_suffix}
 mkdir -p $dir
-echo "$0: Getting CMU dictionary"
-if [ ! -f $dir/cmudict.done ]; then
-  [ -d $dir/cmudict ] && rm -rf $dir/cmudict
-  svn co https://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict $dir/cmudict
-  touch $dir/cmudict.done
-fi
+
+
+# (1) Get the CMU dictionary
+svn co  https://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict \
+  $dir/cmudict || exit 1;
+
+# can add -r 10966 for strict compatibility.
+
+
+#(2) Dictionary preparation:
+
+
+# Make phones symbol-table (adding in silence and verbal and non-verbal noises at this point).
+# We are adding suffixes _B, _E, _S for beginning, ending, and singleton phones.
 
 # silence phones, one per line.
-for w in sil spn inaudible laughs noise; do
-  echo $w;
-done > $dir/silence_phones.txt
-echo sil > $dir/optional_silence.txt
+(echo SIL; echo SPN; echo NSN) > $dir/silence_phones.txt
+echo SIL > $dir/optional_silence.txt
 
-# For this setup we're discarding stress.
-cat $dir/cmudict/cmudict-0.7b.symbols | \
-  perl -ne 's:[0-9]::g; s:\r::; print lc($_)' | \
-  sort -u > $dir/nonsilence_phones.txt
+# nonsilence phones; on each line is a list of phones that correspond
+# really to the same base phone.
+cat $dir/cmudict/cmudict.0.7a.symbols | perl -ane 's:\r::; print;' | \
+ perl -e 'while(<>){
+  chop; m:^([^\d]+)(\d*)$: || die "Bad phone $_"; 
+  $phones_of{$1} .= "$_ "; }
+  foreach $list (values %phones_of) {print $list . "\n"; } ' \
+  > $dir/nonsilence_phones.txt || exit 1;
 
-# An extra question will be added by including the silence phones in one class.
-paste -d ' ' -s $dir/silence_phones.txt > $dir/extra_questions.txt
+# A few extra questions that will be added to those obtained by automatically clustering
+# the "real" phones.  These ask about stress; there's also one for silence.
+cat $dir/silence_phones.txt| awk '{printf("%s ", $1);} END{printf "\n";}' > $dir/extra_questions.txt || exit 1;
+cat $dir/nonsilence_phones.txt | perl -e 'while(<>){ foreach $p (split(" ", $_)) {
+  $p =~ m:^([^\d]+)(\d*)$: || die "Bad phone $_"; $q{$2} .= "$p "; } } foreach $l (values %q) {print "$l\n";}' \
+ >> $dir/extra_questions.txt || exit 1;
 
-grep -v ';;;' $dir/cmudict/cmudict-0.7b |\
-  uconv -f latin1 -t utf-8 -x Any-Lower |\
-  perl -ne 's:(\S+)\(\d+\) :$1 :; s:  : :; print;' |\
-  perl -ne '@F = split " ",$_,2; $F[1] =~ s/[0-9]//g; print "$F[0] $F[1]";' \
+grep -v ';;;' $dir/cmudict/cmudict.0.7a | \
+ perl -ane 'if(!m:^;;;:){ s:(\S+)\(\d+\) :$1 :; print; }' \
   > $dir/lexicon1_raw_nosil.txt || exit 1;
 
-# Add prons for laughter, noise, oov
-for w in `grep -v sil $dir/silence_phones.txt`; do
-  echo "[$w] $w"
-done | cat - $dir/lexicon1_raw_nosil.txt > $dir/lexicon2_raw.txt || exit 1;
+# Add to cmudict the silences, noises etc.
 
-# we keep all words from the cmudict in the lexicon
-# might reduce OOV rate on dev and eval
-cat $dir/lexicon2_raw.txt  \
-   <( echo "mm m"
-      echo "<unk> spn"
-      echo "cuz k aa z"
-      echo "cuz k ah z"
-      echo "cuz k ao z"
-      echo "mmm m"; \
-      echo "hmm hh m"; \
-    ) | sort -u | sed 's/[\t ]/\t/' > $dir/iv_lexicon.txt
+# the sort | uniq is to remove a duplicated pron from cmudict.
+(echo '!SIL SIL'; echo '<SPOKEN_NOISE> SPN'; echo '<UNK> SPN'; echo '<NOISE> NSN'; ) | \
+ cat - $dir/lexicon1_raw_nosil.txt | sort | uniq > $dir/lexicon2_raw.txt || exit 1;
 
 
-cat data/train*/text  | \
-  awk '{for (n=2;n<=NF;n++){ count[$n]++; } } END { for(n in count) { print count[n], n; }}' | \
-  sort -nr > $dir/word_counts
+# lexicon.txt is without the _B, _E, _S, _I markers.
+# This is the input to wsj_format_data.sh
+cp $dir/lexicon2_raw.txt $dir/lexicon.txt
 
-cat $dir/word_counts | awk '{print $2}' > $dir/word_list
+rm $dir/lexiconp.txt 2>/dev/null
 
-awk '{print $1}' $dir/iv_lexicon.txt | \
-  perl -e '($word_counts)=@ARGV;
-   open(W, "<$word_counts")||die "opening word-counts $word_counts";
-   while(<STDIN>) { chop; $seen{$_}=1; }
-   while(<W>) {
-     ($c,$w) = split;
-     if (!defined $seen{$w}) { print; }
-   } ' $dir/word_counts > $dir/oov_counts.txt
-
-echo "*Highest-count OOVs (including fragments) are:"
-head -n 10 $dir/oov_counts.txt
-echo "*Highest-count OOVs (excluding fragments) are:"
-grep -v -E '^-|-$' $dir/oov_counts.txt | head -n 10 || true
-
-echo "*Training a G2P and generating missing pronunciations"
-mkdir -p $dir/g2p/
-phonetisaurus-align --input=$dir/iv_lexicon.txt --ofile=$dir/g2p/aligned_lexicon.corpus
-ngram-count -order 4 -kn-modify-counts-at-end -ukndiscount\
-  -gt1min 0 -gt2min 0 -gt3min 0 -gt4min 0 \
-  -text $dir/g2p/aligned_lexicon.corpus -lm $dir/g2p/aligned_lexicon.arpa
-phonetisaurus-arpa2wfst --lm=$dir/g2p/aligned_lexicon.arpa --ofile=$dir/g2p/g2p.fst
-awk '{print $2}' $dir/oov_counts.txt > $dir/oov_words.txt
-phonetisaurus-apply --nbest 2 --model $dir/g2p/g2p.fst --thresh 5 --accumulate \
-  --word_list $dir/oov_words.txt > $dir/oov_lexicon.txt
-
-## The next section is again just for debug purposes
-## to show words for which the G2P failed
-cat $dir/oov_lexicon.txt $dir/iv_lexicon.txt | sort -u > $dir/lexicon.txt
-rm -f $dir/lexiconp.txt 2>/dev/null; # can confuse later script if this exists.
-awk '{print $1}' $dir/lexicon.txt | \
-  perl -e '($word_counts)=@ARGV;
-   open(W, "<$word_counts")||die "opening word-counts $word_counts";
-   while(<STDIN>) { chop; $seen{$_}=1; }
-   while(<W>) {
-     ($c,$w) = split;
-     if (!defined $seen{$w}) { print; }
-   } ' $dir/word_counts > $dir/oov_counts.g2p.txt
-
-echo "*Highest-count OOVs (including fragments) after G2P are:"
-head -n 10 $dir/oov_counts.g2p.txt
-
-utils/validate_dict_dir.pl $dir
-exit 0;
+echo "Dictionary preparation succeeded"
 
